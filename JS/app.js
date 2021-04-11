c6972f9a6a01c7ce9f1b320b149d8d0510858443
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-04-11 17:44:37 +0300

    lima added

--- JS/app.js
+++ JS/app.js
@@ -35,7 +35,7 @@ for(let i =6 ; i <12 ; i++){
   li1.textContent = `${i}am: ${seattle.cookiesPurchasedPerHour[i]} cookies`;
   total1=total1+seattle.cookiesPurchasedPerHour[i];
 }
-for (let i = 1; i < 7; i++) {
+for (let i = 1; i <8 ; i++) {
   li1 = document.createElement('li');
   ul1.appendChild(li1);
   li1.textContent = `${i}pm: ${seattle.cookiesPurchasedPerHour[i]} cookies`;
@@ -69,7 +69,7 @@ console.log(tokyo.cookiesPurchasedPerHour);
 let bodyTag2 = document.getElementById('body-main');
 let heading2 = document.createElement('h2');
 bodyTag2.appendChild(heading2);
-heading2.textContent = 'tokyo';
+heading2.textContent = 'Tokyo';
 
 let ul2 =document.createElement('ul');
 bodyTag2.appendChild(ul2);
@@ -82,7 +82,7 @@ for(let i =6 ; i <12 ; i++){
   li2.textContent = `${i}am: ${tokyo.cookiesPurchasedPerHour[i]} cookies`;
   total2=total2+tokyo.cookiesPurchasedPerHour[i];
 }
-for (let i = 1; i < 7; i++) {
+for (let i = 1; i <8 ; i++) {
   li2 = document.createElement('li');
   ul2.appendChild(li2);
   li2.textContent = `${i}pm: ${tokyo.cookiesPurchasedPerHour[i]} cookies`;
@@ -115,7 +115,7 @@ console.log(dubai.cookiesPurchasedPerHour);
 let bodyTag3 = document.getElementById('body-main');
 let heading3 = document.createElement('h2');
 bodyTag3.appendChild(heading3);
-heading3.textContent = 'dubai';
+heading3.textContent = 'Dubai';
 
 let ul3 =document.createElement('ul');
 bodyTag3.appendChild(ul3);
@@ -128,7 +128,7 @@ for(let i =6 ; i <12 ; i++){
   li3.textContent = `${i}am: ${dubai.cookiesPurchasedPerHour[i]} cookies`;
   total3=total3+dubai.cookiesPurchasedPerHour[i];
 }
-for (let i = 1; i < 7; i++) {
+for (let i = 1; i <8 ; i++) {
   li3 = document.createElement('li');
   ul3.appendChild(li3);
   li3.textContent = `${i}pm: ${dubai.cookiesPurchasedPerHour[i]} cookies`;
@@ -161,7 +161,7 @@ console.log(paris.cookiesPurchasedPerHour);
 let bodyTag4 = document.getElementById('body-main');
 let heading4 = document.createElement('h2');
 bodyTag4.appendChild(heading4);
-heading4.textContent = 'paris';
+heading4.textContent = 'Paris';
 
 let ul4 =document.createElement('ul');
 bodyTag4.appendChild(ul4);
@@ -174,7 +174,7 @@ for(let i =6 ; i <12 ; i++){
   li4.textContent = `${i}am: ${paris.cookiesPurchasedPerHour[i]} cookies`;
   total4=total4+paris.cookiesPurchasedPerHour[i];
 }
-for (let i = 1; i < 7; i++) {
+for (let i = 1; i <8 ; i++) {
   li4 = document.createElement('li');
   ul4.appendChild(li4);
   li4.textContent = `${i}pm: ${paris.cookiesPurchasedPerHour[i]} cookies`;
@@ -183,3 +183,48 @@ for (let i = 1; i < 7; i++) {
 li4 = document.createElement('li');
 ul4.appendChild(li4);
 li4.textContent = `Total: ${total4} cookies`;
+
+const lima = {
+  min:20,
+  max:38,
+  avgCookies:2.3,
+  randomCusPerHour:[],
+  cookiesPurchasedPerHour:[],
+  cusAvg:function(min,max){
+    for (let i = 0; i < 14; i++) {
+      let cal=(Math.floor(Math.random() * (max - min + 1) + min));
+      let cookiesAmount=Math.floor(cal*this.avgCookies);
+      this.randomCusPerHour.push(cal);
+      this.cookiesPurchasedPerHour.push(cookiesAmount);
+    }
+  }
+};
+lima.cusAvg(lima.min,lima.max);
+console.log(lima.randomCusPerHour);
+console.log(lima.cookiesPurchasedPerHour);
+
+let bodyTag5 = document.getElementById('body-main');
+let heading5 = document.createElement('h2');
+bodyTag5.appendChild(heading5);
+heading5.textContent = 'Lima';
+
+let ul5 =document.createElement('ul');
+bodyTag5.appendChild(ul5);
+
+let li5=null;
+let total5=0;
+for(let i =6 ; i <12 ; i++){
+  li5 = document.createElement('li');
+  ul5.appendChild(li5);
+  li5.textContent = `${i}am: ${lima.cookiesPurchasedPerHour[i]} cookies`;
+  total5=total5+lima.cookiesPurchasedPerHour[i];
+}
+for (let i = 1; i <8 ; i++) {
+  li5 = document.createElement('li');
+  ul5.appendChild(li5);
+  li5.textContent = `${i}pm: ${lima.cookiesPurchasedPerHour[i]} cookies`;
+  total5=total5+lima.cookiesPurchasedPerHour[i];
+}
+li5 = document.createElement('li');
+ul5.appendChild(li5);
+li5.textContent = `Total: ${total5} cookies`;
