141a5b92aec44526d5a0cbcdd5a56f9557ab0142
nawal-ahmad <nawalahmad.bme@gmail.com>
2021-04-11 17:34:46 +0300

    tokyo and dubai added

--- JS/app.js
+++ JS/app.js
@@ -93,3 +93,47 @@ ul2.appendChild(li2);
 li2.textContent = `Total: ${total2} cookies`;
 
 
+const dubai = {
+  min:11,
+  max:38,
+  avgCookies:3.7,
+  randomCusPerHour:[],
+  cookiesPurchasedPerHour:[],
+  cusAvg:function(min,max){
+    for (let i = 0; i < 14; i++) {
+      let cal=(Math.floor(Math.random() * (max - min + 1) + min));
+      let cookiesAmount=Math.floor(cal*this.avgCookies);
+      this.randomCusPerHour.push(cal);
+      this.cookiesPurchasedPerHour.push(cookiesAmount);
+    }
+  }
+};
+dubai.cusAvg(dubai.min,dubai.max);
+console.log(dubai.randomCusPerHour);
+console.log(dubai.cookiesPurchasedPerHour);
+
+let bodyTag3 = document.getElementById('body-main');
+let heading3 = document.createElement('h2');
+bodyTag3.appendChild(heading3);
+heading3.textContent = 'dubai';
+
+let ul3 =document.createElement('ul');
+bodyTag3.appendChild(ul3);
+
+let li3=null;
+let total3=0;
+for(let i =6 ; i <12 ; i++){
+  li3 = document.createElement('li');
+  ul3.appendChild(li3);
+  li3.textContent = `${i}am: ${dubai.cookiesPurchasedPerHour[i]} cookies`;
+  total3=total3+dubai.cookiesPurchasedPerHour[i];
+}
+for (let i = 1; i < 7; i++) {
+  li3 = document.createElement('li');
+  ul3.appendChild(li3);
+  li3.textContent = `${i}pm: ${dubai.cookiesPurchasedPerHour[i]} cookies`;
+  total3=total3+dubai.cookiesPurchasedPerHour[i];
+}
+li3 = document.createElement('li');
+ul3.appendChild(li3);
+li3.textContent = `Total: ${total3} cookies`;
